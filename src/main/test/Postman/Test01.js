54da06f9fed06713e6ca26fc9e576a0e9451b779
bilalaymak <bilalaymak@gmail.com>
2023-04-07 01:13:21 +0300

    Postman API updated

--- src/main/test/Postman/Test01.js
+++ src/main/test/Postman/Test01.js
@@ -1,15 +1,15 @@
 const assert = require('chai').assert;
 
-// Test sınıfı
+// Test class
 describe('MyTestClass', function() {
 
-  // Test yöntemi
+  // Test method
   it('should return true when the value is true', function() {
     const value = true;
     assert.isTrue(value);
   });
 
-  // Başka bir test yöntemi
+  // test method
   it('should return false when the value is false', function() {
     const value = false;
     assert.isFalse(value);
