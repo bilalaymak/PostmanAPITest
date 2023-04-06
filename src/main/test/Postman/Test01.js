e78216d15691c7b6b358f2b4d39f012a653bee62
bilalaymak <bilalaymak@gmail.com>
2023-04-07 01:45:06 +0300

    Postman API updated

--- src/main/test/Postman/Test01.js
+++ src/main/test/Postman/Test01.js
@@ -25,3 +25,64 @@ pm.test("validation of fields ", ()=>{
     pm.expect(jsonData.totalprice).to.eql(111);
     pm.expect(jsonData.depositpaid).to.eql(true);
 });
+
+
+//validation json fields in Response
+pm.test("validation of fields ", ()=>{
+    var jsonData = pm.response.json();
+    pm.expect(jsonData.firstname).to.eql("Josh");
+    pm.expect(jsonData.lastname).to.eql("Allen");
+    pm.expect(jsonData.totalprice).to.eql(111);
+    pm.expect(jsonData.depositpaid).to.eql(true);
+});
+
+//json schema validation
+pm.test('schema is valid', function(){
+    var jsonData = pm.response.json();
+var schema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "firstname": {
+      "type": "string"
+    },
+    "lastname": {
+      "type": "string"
+    },
+    "totalprice": {
+      "type": "integer"
+    },
+    "depositpaid": {
+      "type": "boolean"
+    },
+    "bookingdates": {
+      "type": "object",
+      "properties": {
+        "checkin": {
+          "type": "string"
+        },
+        "checkout": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "checkin",
+        "checkout"
+      ]
+    },
+    "additionalneeds": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "firstname",
+    "lastname",
+    "totalprice",
+    "depositpaid",
+    "bookingdates",
+    "additionalneeds"
+  ]
+}
+
+    pm.expect(tv4.validate(jsonData, schema)).to.be.true;
+});
